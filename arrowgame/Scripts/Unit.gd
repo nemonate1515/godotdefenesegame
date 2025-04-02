7a264004710a493507a0b7120b8e63d19fed0f3f
nemonate1515 <roqkf1515@gmail.com>
2025-04-02 23:33:32 +0900

    Add files via upload

---
+++ arrowgame/Scripts/Unit.gd
@@ -0,0 +1,18 @@
+class_name Unit
+extends Area2D
+
+@export var stats: UnitStats : set = set_stats
+
+@onready var skin: Sprite2D = $Skin
+@onready var mana_bar: ProgressBar = $ManaBar
+
+func set_stats(value: UnitStats) -> void:
+	stats = value
+	
+	if value == null:
+		return
+	
+	if not is_node_ready():
+		await ready
+	
+	skin.region_rect.position = Vector2(stats.skin_coordinates) * Arena.CELL_SIZE
